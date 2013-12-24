2bf39a82515669ff82c7d02f08c46f42dc4c4fa9
Sahat Yalkabov <sakhat@gmail.com>
2013-12-24 16:31:59 -0500

    Moved token code to pascal.js

--- pascal.js
+++ pascal.js
@@ -28,6 +28,35 @@
   // Current version.
   Pascal.VERSION = '0.1.1';
 
+  var Token = function (value, type) {
+    this.tokenValue = value;
+    this.tokenType = type;
+    this.lineNumber = -1;
+  };
+
+  Token.T_IDENTIFIER = 0;
+  Token.NUMBER = 1;
+  Token.SYMBOL = 2; // T_OP
+  Token.T_SPECIAL = 7;
+
+  Token.COMMENT = 3;
+  Token.STRING = 4;
+  Token.EOF = 5;
+  Token.RESERVED = 6;
+
+  Token.prototype.isEqualTo = function(second) {
+    return this.tokenType === second.tokenType &&
+      this.tokenValue === second.tokenValue;
+  };
+
+  Token.prototype.isSymbol = function(symbol) {
+    return this.tokenType === Token.SYMBOL && this.tokenValue === symbol;
+  };
+
+  Token.prototype.isReserved = function(reservedWord) {
+    return this.tokenType === Token.RESERVED &&
+      this.tokenValue.toLowerCase() === reservedWord.toLowerCase();
+  };
 
   // AMD registration happens at the end for compatibility with AMD loaders
   // that may not enforce next-turn semantics on modules. Even though general
