87d9d4647ba7d6c0ab7c83302c960fc31d83db9c
Sahat Yalkabov <sakhat@gmail.com>
2014-01-13 20:38:32 -0500

    Cleanup

---
+++ Token.js
@@ -0,0 +1,34 @@
+/**
+ * Token
+ * @param value
+ * @param type
+ */
+
+var Token = function(value, type) {
+  this.tokenValue = value;
+  this.tokenType = type;
+  this.line = -1;
+
+  this.isEqualTo = function(second) {
+    return this.tokenType === second.tokenType && this.tokenValue === second.tokenValue;
+  };
+
+  this.isSymbol = function(symbol) {
+    return this.tokenType === Token.TK_SYMBOL && this.tokenValue === symbol;
+  };
+
+  this.isReserved = function(reservedWord) {
+    return this.tokenType === Token.TK_RESERVED &&
+      this.tokenValue.toLowerCase() === reservedWord.toLowerCase();
+  };
+};
+
+Token.TK_IDENTIFIER = 0;
+Token.TK_NUMBER = 1;
+Token.TK_SYMBOL = 2;
+Token.TK_COMMENT = 3;
+Token.TK_STRING = 4;
+Token.TK_EOF = 5;
+Token.TK_RESERVED = 6;
+
+module.exports = Token;
