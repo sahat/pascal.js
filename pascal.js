328c1767645b78fe71a117b0e89cedfaa614d7fc
Sahat Yalkabov <sakhat@gmail.com>
2014-01-13 23:14:26 -0500

    Load file via command line argument

--- pascal.js
+++ pascal.js
@@ -4,30 +4,21 @@
  * @author Sahat Yalkabov
  * @license MIT
  */
+
 var _ = require('underscore');
 var fs = require('fs');
-var PascalError = require('./pascal_error');
 var Token = require('./token');
-var Bytecode = require('./bytecode');
 var Scanner = require('./scanner');
 var Stream = require('./stream');
-var Symbol = require('./symbol');
-var Node = require('./node');
 var CommentStripper = require('./comment_stripper');
-var RawData = require('./raw_data');
-var OPCODES = require('./opcodes');
-var NativeProcedure = require('./native_procedure');
-var Native = require('./native');
-var SymbolLookup = require('./symbol_lookup')
 var SymbolTable = require('./symbol_table');
 var Compiler = require('./compiler');
 var Machine = require('./machine');
 var Parser = require('./parser');
 
 
-
-var contents = fs.readFileSync('./examples/hello.pas', 'utf8');
-
+var filePath = process.argv[2];
+var contents = fs.readFileSync(filePath, 'utf8');
 var stream = new Stream(contents);
 var scanner = new CommentStripper(new Scanner(stream));
 var parser = new Parser(scanner);
