3fa2eb261fe53965d0466ef2c32b3ea1c27ebc5a
Sahat Yalkabov <sakhat@gmail.com>
2014-02-26 20:45:59 -0500

    Added scanner spec stubs

---
+++ test/scannerSpec.js
@@ -0,0 +1,34 @@
+var should = require('chai').should();
+var Token = require('../lib/modules/token.js');
+
+describe('Token', function() {
+
+  before(function(done) {
+    // read pascal file
+  });
+
+  it('should find next character', function() {
+
+  });
+
+  it('should perform character lookahead', function() {
+
+  });
+
+  it('should find next token', function() {
+
+  });
+
+  it('should return previous character', function() {
+
+  });
+
+  it('should find the longest token', function() {
+
+  });
+
+  it('should find next token', function() {
+
+  });
+
+});
