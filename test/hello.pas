548993e8d898a1ebf82b9e6d6d27baa51e8a115f
Sahat Yalkabov <sakhat@gmail.com>
2014-03-12 22:33:30 -0400

    Added compiler unit test that runs an example; updated example code.

--- test/hello.pas
+++ test/hello.pas
@@ -4,5 +4,4 @@ program helloworld;
 
 begin
     Writeln('Hello world!');
-    if (5 > 4) and (3 < 11) then Writeln('Gr.4 and Lt.11');
 end.
---
+++ test/pascalSpec.js
@@ -0,0 +1,30 @@
+var should = require('chai').should();
+var _ = require('underscore');
+var fs = require('fs');
+var Token = require('../lib/modules/token');
+var Scanner = require('../lib/modules/scanner');
+var CommentStripper = require('../lib/modules/comment_stripper');
+var SymbolTable = require('../lib/modules/symbol_table');
+var Compiler = require('../lib/modules/compiler');
+var Machine = require('../lib/modules/machine');
+var Parser = require('../lib/modules/parser');
+
+describe('Pascal Compiler', function() {
+
+  var fileData = fs.readFileSync('./test/hello.pas', 'utf8');
+  var scanner = new CommentStripper(new Scanner(fileData));
+  var parser = new Parser(scanner);
+  var builtinSymbolTable = SymbolTable.makeBuiltinSymbolTable();
+  var root = parser.parse(builtinSymbolTable);
+  var compiler = new Compiler();
+  var bytecode = compiler.compile(root);
+  var machine = new Machine(bytecode);
+
+  it('should run the pascal example', function() {
+    machine.setOutputCallback(function(line) {
+      line.should.equal('Hello world!');
+    });
+    machine.run();
+  });
+
+});
