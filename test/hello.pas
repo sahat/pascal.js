3a209e847b1ae6965daa7c911fec04df8fb0e133
Sahat Yalkabov <sakhat@gmail.com>
2014-05-13 01:07:41 -0400

    More refactoring

--- test/hello.pas
+++ test/hello.pas
@@ -20,4 +20,15 @@ begin
     result := number1 + number2;
     writeln(number1, ' plus ', number2, ' is ', result )
 
+    number1 := 2;
+    number2 := 2;
+
+    writeln('Two times two is ', number1 * number2);
+
+    number1 := 9;
+    number2 := 3;
+
+    writeln('9 divided by 3 is  ', number1 div number2);
+
+
 end.
