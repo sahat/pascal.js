ec01ff77064e9067d08136df924c7983c2eabe1e
Sahat Yalkabov <sakhat@gmail.com>
2014-05-13 00:04:17 -0400

    Merged built-in into symbol table

--- test/hello.pas
+++ test/hello.pas
@@ -4,8 +4,16 @@ program helloworld;
 
 var number1, number2, result : integer;
 
+type my_list_of_names = array[0..7] of string;
+
+var the_list: my_list_of_names;
+
 begin
-    Writeln('Hello world!');
+
+    the_list[0] := 'Newbie';
+
+    writeln('Hello world!');
+    writeln('Name 5: ', the_list[0]);
 
     number1 := 10;
     number2 := 20;
