0ac0ca34382cafc32932d9f7188b9b77e1be7bde
Sahat Yalkabov <sakhat@gmail.com>
2014-05-15 20:18:36 -0400

    Compiler refactoring

--- test/hello.pas
+++ test/hello.pas
@@ -1,24 +1,13 @@
 program helloworld;
 
 { Print hello world }
+
 var sum, counter: integer;
 var number1, number2, result : integer;
-
-type my_list_of_names = array[0..7] of string;
-
-var the_list: my_list_of_names;
-
 Var counter : integer;
 
-begin
-
-
-    the_list[0] := 'Newbie';
-    the_list[1] := 'Second name';
-
+Begin
     writeln('Hello world!');
-    writeln('Name 0: ', the_list[0]);
-    writeln('Name 1: ', the_list[1]);
 
     number1 := 10;
     number2 := 20;
@@ -42,4 +31,4 @@ begin
 
     for counter := 1 to 7 do
       writeln('Counter - ', counter);
-end.
+End
